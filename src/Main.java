b0dd1cbc94fa89de224322b23d12d29cb784be07
PolinaRrr <poniapocalypsisa@gmail.com>
2020-10-05 13:37:31 +0300

    Commit #3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
 public class Main {
+
+    static final Object monitor = new Object();
+    static volatile char letter = 'A';
+
+    static class PrintLetter implements Runnable {
+        private char actLetter;
+        private char secLetter;
+
+        public PrintLetter(char actLetter, char secLetter) {
+            this.actLetter = actLetter;
+            this.secLetter = secLetter;
+        }
+
+        @Override
+        public void run() {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    synchronized (monitor) {
+                        while (letter != actLetter) {
+                            monitor.wait();
+                        }
+                        System.out.println(actLetter);
+                        letter = secLetter;
+                        monitor.notifyAll();
+                    }
+
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void main(String[] args) {
+        new Thread(new PrintLetter('A', 'B')).start();
+        new Thread(new PrintLetter('B', 'C')).start();
+        new Thread(new PrintLetter('C', 'A')).start();
 
     }
 }
